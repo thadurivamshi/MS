28aaac83de1f38ec697231092c5ca417fa7cab44
thadurivamshi <tvamshi292001@gmail.com>
2022-04-13 22:04:15 +0530

    third cioommit

--- app/src/main/java/com/shashankesh/ms/StartActivity.java
+++ app/src/main/java/com/shashankesh/ms/StartActivity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.VideoView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,6 +21,11 @@ public class StartActivity extends AppCompatActivity {
     Button mNeedAcc;
     Button mLogin;
     private FirebaseAuth mAuth;
+    // Create a VideoView variable, a MediaPlayer variable, and an int to hold the current
+    // video position.
+    private VideoView videoBG;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,19 @@ public class StartActivity extends AppCompatActivity {
         mLogin = (Button)findViewById(R.id.login);
         mNeedAcc = (Button)findViewById(R.id.need_acc);
         mAuth = FirebaseAuth.getInstance();
+        // Hook up the VideoView to our UI.
+        videoBG = (VideoView) findViewById(R.id.videoView);
+        // Build your video Uri
+        Uri uri = Uri.parse("android.resource://" // First start with this,
+                + getPackageName() // then retrieve your package name,
+                + "/" // add a slash,
+                + R.raw.water); // and then finally add your video resource. Make sure it is stored
+        // in the raw folder.
+
+        // Set the new Uri to our VideoView
+        videoBG.setVideoURI(uri);
+        // Start the VideoView
+        videoBG.start();
 
         mNeedAcc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,8 +63,33 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(startInt);
             }
         });
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mMediaPlayer = mediaPlayer;
+                // We want our video to play over and over so we set looping to true.
+                mMediaPlayer.setLooping(true);
+                // We then seek to the current posistion if it has been set and play the video.
+                if (mCurrentVideoPosition != 0) {
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
+            }
+        });
     }
     @Override
+    protected void onPause() {
+        super.onPause();
+        // Capture the current video position and pause the video.
+        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
+        videoBG.pause();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Restart the video when resuming the Activity
+        videoBG.start();
+    }
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
@@ -54,5 +103,12 @@ public class StartActivity extends AppCompatActivity {
             startActivity(startIntent);
             finish();
         }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // When the Activity is destroyed, release our MediaPlayer and set it to null.
+        mMediaPlayer.release();
+        mMediaPlayer = null;
     }
 }
